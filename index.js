b1560d22d44fc874590ebf6ea1f84366a6597421
Utsavkumar Lal <utsav.lal@makkajai.com>
2022-01-27 17:11:32 +0530

    Added basic plugin features for testing

--- index.js
+++ index.js
@@ -1,22 +1,57 @@
-// <TODO: your plugin code here - you can base it on the code below, but you don't have to>
+import { Plugin, PluginEvent, PluginMeta } from '@posthog/plugin-scaffold'
+import { Client, QueryResult, QueryResultRow } from 'pg'
 
-// Some internal library function
-async function getRandomNumber() {
-    return 4
-}
 
 // Plugin method that runs on plugin load
 export async function setupPlugin({ config }) {
-    console.log(`Setting up the plugin`)
+    const requiredConfigOptions = ['clusterHost', 'clusterPort', 'dbName', 'dbUsername', 'dbPassword']
+    for (const option of requiredConfigOptions) {
+        if (!(option in config)) {
+            throw new Error(`Required config option ${option} is missing!`)
+        }
+    }
+
+    if (!config.clusterHost.endsWith('redshift.amazonaws.com')) {
+        throw new Error('Cluster host must be a valid AWS Redshift host')
+    }
+
+    const totalRowsResult = await executeQuery(
+        `SELECT COUNT(1) FROM ${sanitizeSqlIdentifier(config.tableName)}`,
+        [],
+        config
+    )
+
+    if (!totalRowsResult || totalRowsResult.error || !totalRowsResult.queryResult) {
+        throw new Error('Unable to connect to Redshift!')
+    }
 }
 
 // Plugin method that processes event
 export async function processEvent(event, { config, cache }) {
-    const counterValue = (await cache.get('greeting_counter', 0))
-    cache.set('greeting_counter', counterValue + 1)
-    if (!event.properties) event.properties = {}
-    event.properties['greeting'] = config.greeting
-    event.properties['greeting_counter'] = counterValue
-    event.properties['random_number'] = await getRandomNumber()
+    console.log(event)
     return event
+}
+
+const executeQuery = async (query,values,config) => {
+    const pgClient = new Client({
+        user: config.dbUsername,
+        password: config.dbPassword,
+        host: config.clusterHost,
+        database: config.dbName,
+        port: parseInt(config.clusterPort),
+    })
+
+    await pgClient.connect()
+
+    let error = null
+    let queryResult = null
+    try {
+        queryResult = await pgClient.query(query, values)
+    } catch (err) {
+        error = err
+    }
+
+    await pgClient.end()
+
+    return { error, queryResult }
 }
